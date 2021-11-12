b340503183225a72c035e9f46f5da7baddb1895b
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-11-12 09:33:09 -0300

    Requisito 4 - correção lint

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state= {
+      inputArtist: '',
+    };
+  }
+
   render() {
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="inputArtist"
+            id="search-artist-input"
+            placeholder="Nome do Artista"
+            onChange={ this.handleInputChange }
+            value={ inputArtist }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ isDisabled }
+            onClick={ this.handleClickButton }
+            value={ inputName }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
