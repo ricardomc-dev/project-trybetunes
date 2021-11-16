a228551b8835cf674f94b57f17e740bcf0f12b59
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-11-16 15:48:44 -0300

    Requisito 7 e 8

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
+
+class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isChecked: false,
+      isLoading: false,
+    };
+    this.handleFavoritesSongs = this.handleFavoritesSongs.bind(this);
+  }
+
+  handleFavoritesSongs({ target: { checked } }) {
+    const { music } = this.props;
+    this.setState({ isLoading: true });
+    if (checked) {
+      addSong(music).then(() => this.setState({ isLoading: false, isChecked: true }));
+    }
+  }
+
+  render() {
+    const { trackName, previewUrl, trackId } = this.props;
+    const { isChecked, isLoading } = this.state;
+    return (
+      <div>
+        {isLoading ? <Loading /> : (
+          <>
+            <p>{ trackName }</p>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              <code>audio</code>
+            </audio>
+            <label htmlFor={ trackId }>
+              Favorita
+              <input
+                type="checkbox"
+                data-testid={ `checkbox-music-${trackId}` }
+                name="favoriteSong"
+                id={ trackId }
+                onChange={ this.handleFavoritesSongs }
+                checked={ isChecked }
+              />
+            </label>
+          </>
+
+        )}
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+  music: PropTypes.shape({
+    trackId: PropTypes.number,
+  }).isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      musics: [],
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    getMusics(id)
+      .then((data) => this.setState({ musics: data },
+        () => this.setState({ isLoading: false })));
+  }
+
   render() {
+    const { musics, isLoading } = this.state;
+
+    const renderName = !isLoading
+      && <p data-testid="artist-name">{musics[0].artistName}</p>;
+    const renderAlbumName = !isLoading
+      && <p data-testid="album-name">{musics[0].collectionName}</p>;
+
     return (
       <div data-testid="page-album">
         <Header />
+        { isLoading && <Loading /> }
+        { renderName }
+        { renderAlbumName }
+        { !isLoading && musics.filter((music) => music.trackId !== undefined)
+          .map((music) => (<MusicCard
+            key={ music.trackId }
+            trackName={ music.trackName }
+            previewUrl={ music.previewUrl }
+            trackId={ music.trackId }
+            music={ music }
+          />))}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
